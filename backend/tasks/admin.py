48ac55b3268e67870866a7360884817a3a9102cd
MIZERO Adrien <adrienmizero@gmail.com>
2026-09-16 18:24:25 +0200

    feat: register Task model in Django admin

---
+++ backend/tasks/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'status', 'priority', 'created_at', 'updated_at']
+    list_filter = ['status', 'priority']
+    search_fields = ['title', 'description']
